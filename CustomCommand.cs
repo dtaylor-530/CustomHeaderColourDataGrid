003949949de6b02917718f87031d213f03aa28a5
dtaylor-530 <d.taylor530@gmail.com>
2019-02-09 17:31:20 +0000

    Initial commit

---
+++ CustomCommand.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+using System.Windows.Shapes;
+using Xceed.Wpf.Toolkit;
+
+namespace WpfCustomHeaderGrid
+{
+    public class CustomCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        public  bool CanExecute()
+        {
+            return true;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+
+        public void Execute(object parameter)
+        {
+            Tuple<Control, ListBox> t = (parameter as Tuple<Control, ListBox>);
+            (t.Item1).Background = (((t.Item2 as ListBox).SelectedItem as ListBoxItem).Content as Ellipse).Fill;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfCustomHeaderGrid {
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+            dataGrid.Loaded += dataGrid_Loaded;
+        }
+
+        void dataGrid_Loaded(object sender, RoutedEventArgs e) {
+            dataGrid.Loaded -= dataGrid_Loaded;
+            dataGrid.ItemsSource =           Enumerable.Range(0, 10).Select(_ => NewDataRow()).ToList();
+        }
+        private DataRow NewDataRow() => new DataRow
+        {
+            aas = Faker.CompanyFaker.BS(),
+            aaass = Faker.LocationFaker.City(),
+            aasas = Faker.NameFaker.FemaleFirstName(),
+            addas = Faker.PhoneFaker.InternationalPhone()
+        };
+
+
+    }
+
+}
+
+
+
+public class DataRow
+{
+    public string aas { get; set; }
+    public string aaass { get; set; }
+    public string aasas { get; set; }
+    public string addas { get; set; }
+}
+
+
